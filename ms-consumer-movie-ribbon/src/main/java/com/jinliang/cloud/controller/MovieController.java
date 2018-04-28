1e97546d74bf919c2d098cab00440baa4f05858f
金亮 <liang.jin@hand-china.com>
2018-04-28 11:33:02 +0800

    add ribbon to ms-consumer-movie

--- ms-consumer-movie-ribbon/src/main/java/com/jinliang/cloud/controller/MovieController.java
+++ ms-consumer-movie-ribbon/src/main/java/com/jinliang/cloud/controller/MovieController.java
@@ -1,7 +1,11 @@
 package com.jinliang.cloud.controller;
 
 import com.jinliang.cloud.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +16,22 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     RestTemplate restTemplate;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
-        return restTemplate.getForObject("http://localhost:8000/"+id,User.class);
+        return restTemplate.getForObject("http://ms-provider-user/"+id,User.class);
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("ms-provider-user");
+        MovieController.LOGGER.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+
+
     }
 }
