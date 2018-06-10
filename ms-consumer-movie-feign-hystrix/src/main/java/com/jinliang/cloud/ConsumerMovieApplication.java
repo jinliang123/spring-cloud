b2bc4ddb9bdc838dfffc5c62cde68dc306a6c84b
金亮 <liang.jin@hand-china.com>
2018-06-10 15:31:37 +0800

    dashboard

--- ms-consumer-movie-feign-hystrix/src/main/java/com/jinliang/cloud/ConsumerMovieApplication.java
+++ ms-consumer-movie-feign-hystrix/src/main/java/com/jinliang/cloud/ConsumerMovieApplication.java
@@ -2,12 +2,15 @@ package com.jinliang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ConsumerMovieApplication {
 
 	public static void main(String[] args) {
---
+++ ms-hystrix-dashboard/src/main/java/com/jinliang/cloud/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.jinliang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Auther: jinliang
+ * Date: 2018/6/10 14:55
+ * Description:
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class,args);
+    }
+}
